ee59ff1677cfb30d831874b20e8cc91fe5a7c1ee
DimaOliinyk <Oliinyk.Dmytro2@lll.kpi.ua>
2026-02-12 16:21:54 +0200

    All blocks added

---
+++ ModelSimulation/ModelSimulation/Blocks.fs
@@ -0,0 +1,40 @@
+﻿module Blocks
+    open System.Collections.Generic
+
+    let Gain (gainCoefficient: double) (x: double) = 
+        gainCoefficient * x 
+
+    let Integrate (dt: double) = 
+        let mutable prev = double 0.
+        let mutable sum = double 0.
+        fun (x: double) -> 
+            sum <- (prev + x) * dt / 2. + sum
+            prev <- x 
+            sum
+
+    let Derive (dt: double)  = 
+        let mutable prev = 0.
+        fun (x: double) -> 
+            let y = (x - prev) / dt 
+            prev <- x 
+            y
+
+    let Aperiodic (dt: double) (timeConstant: double) = 
+        let mutable prev = 0.
+        fun (x: double) -> 
+            let y = (dt * x + timeConstant * prev) / (timeConstant + dt)
+            prev <- y 
+            y
+
+    let Delay (dt: double) (timeDelay: double) = 
+        let delayQueue = Queue<double>()
+        let cnt = int (timeDelay / dt)
+
+        if cnt < 1 then None
+        else Some(
+            fun (x: double) ->
+                delayQueue.Enqueue(x)
+                if delayQueue.Count > cnt then
+                    delayQueue.Dequeue()
+                else 
+                    0)
---
+++ ModelSimulation/ModelSimulation/Program.fs
@@ -0,0 +1,47 @@
+﻿open Blocks
+
+//let input = [1.0; 2; 3; 4; 5; 6; 7; 8; 9;]
+let input = [1.; 1; 1; 1; 1; 1; 1; 1; 1;]
+
+printfn "\nGain block"
+let myGain = Gain -0.5
+
+input 
+|> List.map (fun x -> myGain x) 
+|> List.iter (fun x -> printf "%f, " x) 
+
+printfn "\nIntegrator block"
+let myInt = Integrate 1.
+
+input 
+|> List.map (fun x -> myInt x) 
+|> List.iter (fun x -> printf "%f, " x) 
+
+printfn "\nDerivative block"
+let myDiv = Derive 1.
+
+input 
+|> List.map (fun x -> myDiv x) 
+|> List.iter (fun x -> printf "%f, " x) 
+
+printfn "\nAperiodic block"
+let myAp = Aperiodic 1. 1.
+
+input 
+|> List.map (fun x -> myAp x) 
+|> List.iter (fun x -> printf "%f, " x) 
+
+printfn "\nDelay block"
+let myDelay = Option.get (Delay 1. 5.)
+
+input 
+|> List.map (fun x -> myDelay x) 
+|> List.iter (fun x -> printf "%f, " x) 
+
+printfn "\nGain of 2 and delay of 2 and integrator"
+let myNewInt = Integrate 1.
+let myNewDelay = Option.get (Delay 1. 2.)
+let myTransferFunc = myGain >> myNewDelay >> myNewInt
+input 
+|> List.map (fun x -> myTransferFunc x)
+|> List.iter (fun x -> printf "%f, " x) 
