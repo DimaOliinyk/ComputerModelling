de33de183ab2a55e46bd410550f5b91b70427c81
DimaOliinyk <Oliinyk.Dmytro2@lll.kpi.ua>
2026-02-20 21:27:38 +0200

    PID experiments

---
+++ ModelSimulation/ModelSimulation/Controllers.fs
@@ -0,0 +1,14 @@
+﻿module Controllers
+
+    open Blocks
+
+    let PID (dt: double) (pGain: double) (iGain: double) (dGain: double) =
+        let intg = Integrate dt
+        let derv = Derive dt
+        
+        fun (x: double) ->  
+            Gain pGain x + 
+            Gain dGain (derv x) + 
+            Gain iGain (intg x) 
+            
+
